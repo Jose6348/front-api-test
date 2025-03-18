ac5c9d6c30a018b8e42602d39cf7b95121589b1b
Jose6348 <jfalasco88@gmail.com>
2025-03-18 15:09:45 -0300

    Configuração inicial do projeto Vue.js com dependências necessárias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8084,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/oauth': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })
